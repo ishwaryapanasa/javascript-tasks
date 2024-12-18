f5494d42cecfab704efb0017613cf4391930edd8
ishwaryapanasa <ishwaryapanasa2@gmail.com>
2024-12-18 12:28:03 +0530

    react_projects

---
+++ react-project-2/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Navigation from './components/navigation/Navigation';
+import Contact from './components/contact/Contact';
+
+
+function App() {
+ 
+
+  return (
+    <div>
+   <Navigation/>
+   <Contact/>
+   </div>
+  );
+}
+
+export default App
---
+++ react-project-2/src/components/contact/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from'./Contact.module.css';
+
+const Contact = () => {
+  return (
+    <div className={styles.contact}>
+      <h1>CONTACT US</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, doloremque! Consequuntur, qui maxime voluptas exercitationem ea ullam? Est tenetur fuga, perspiciatis nisi mollitia dolorem? A facere vitae facilis beatae veritatis!</p>
+    </div>
+  )
+}
+
+export default Contact
